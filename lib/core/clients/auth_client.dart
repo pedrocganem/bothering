c9181a4ce2f7d62e28fb980b9cce313dc1564a4a
Pedro Ganem <pedrocastilho.ganemg@gmail.com>
2022-01-30 22:00:21 -0300

    implements authentication client

---
+++ lib/core/clients/auth_client.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthClient {
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        debugPrint('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        debugPrint('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> register(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        debugPrint('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        debugPrint('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> signInAnonymously() async {
+    await _auth.signInAnonymously();
+  }
+
+  Future<void> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
---
+++ lib/core/clients/storage_client.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageClient {
+  final _firestore = FirebaseStorage.instance;
+
+  Future<void> createUser() async {
+
+  }
+}
